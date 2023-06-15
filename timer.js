18b6060941b623aacf120d431fa21bdf9a7284a6
vatsavaineha06 <82636727+vatsavaineha06@users.noreply.github.com>
2023-06-15 21:59:26 +0530

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,29 @@
+function updateTimer(date){
+
+		var now = new Date().getTime();
+		var distance = date - now;
+
+		// Time calculations for days, hours, minutes and seconds
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		// select element
+		document.querySelector('.clock-day').innerHTML = days;
+		document.querySelector('.clock-hours').innerHTML = hours;
+		document.querySelector('.clock-minutes').innerHTML = minutes;
+		document.querySelector('.clock-seconds').innerHTML = seconds;
+
+		if(now >= date){
+			clearInterval(startTimer);
+			document.querySelector('.clock-day').innerHTML = 'D';
+			document.querySelector('.clock-hours').innerHTML = 'O';
+			document.querySelector('.clock-minutes').innerHTML = 'N';
+			document.querySelector('.clock-seconds').innerHTML = 'E';
+		}
+	}
+
+	startTimer = setInterval(function() { updateTimer(date); }, 1000);
+
+}
